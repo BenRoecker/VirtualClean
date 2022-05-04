cc9c383af569ed970d6d9503b7e2ed42e5226124
Sbyre-XII <61010983+Sbyre-XII@users.noreply.github.com>
2022-05-04 16:38:30 +0200

    New Asset Trash

--- HelloQuest/Assets/Script/TrashManager.cs
+++ HelloQuest/Assets/Script/TrashManager.cs
@@ -6,7 +6,14 @@ public class TrashManager : MonoBehaviour
 {
     public static TrashManager m_instance;
 
-    public GameObject m_trash;
+    [Header("Trash Can")]
+
+    public GameObject m_trashCan2;
+    public GameObject m_trashCanSmall;
+    public GameObject m_trashCanTall2;
+    public GameObject m_trashCanTall;
+    public GameObject m_trashCan;
+    public GameObject m_trashSodaCan;
 
     public void Start()
     {
@@ -18,13 +25,13 @@ public class TrashManager : MonoBehaviour
         //Creation of the trashes
         for (int i = 0; i < 1; i++) 
         {
-            CreateTrash(i);
+            CreateTrash(i, m_trashCan2);
         }
     }
 
-    public void CreateTrash(int pos)
+    public void CreateTrash(int pos, GameObject trash)
     {
-        GameObject m_newTrash = Instantiate(m_trash);
+        GameObject m_newTrash = Instantiate(trash);
         m_newTrash.transform.position = new Vector3(pos + 3, 0, 0);
     }
 }
